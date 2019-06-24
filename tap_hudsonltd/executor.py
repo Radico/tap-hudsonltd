d0b20bcd47be129a9627c58e988dac22f42e5547
imcinerney <ian@simondata.com>
2019-06-24 17:31:25 -0400

    Change requests to pull daily instead of biweekly

--- tap_hudsonltd/executor.py
+++ tap_hudsonltd/executor.py
@@ -7,6 +7,8 @@ import base64
 import pendulum
 from xml.etree import ElementTree
 from bs4 import BeautifulSoup
+import time
+
 
 LOGGER = singer.get_logger()
 
@@ -25,23 +27,47 @@ class HudsonltdExecutor(TapExecutor):
         self.replication_key_format = 'datetime_string'
         self.url = (f'https://{self.config.get("accountdomain")}.hudsonltd.'
                     f'net/api/ReadRecords')
+        self.MAX_RECORDS = 10000
 
     def call_full_stream(self, stream):
         """Extract a full stream"""
+        start_date, end_date = self.intialize_dates(self)
+
         request_config = {
             'url': self.url,
             'headers': self.build_headers(),
             'run': True,
-            'data': self.build_body(stream)
+            'data': self.build_body(stream, start_date)
         }
 
-        res = self.client.make_request(request_config, method='POST')
+        while request_config['run']:
+            print(f'the start date is {start_date} and the end date is {end_date}')
+            time.sleep(2)
+            res = self.client.make_request(request_config, method='POST')
 
-        if res.status_code != 200:
-            raise AttributeError(f'Received status_code {res.status_code}')
+            if res.status_code != 200:
+                raise AttributeError(f'Received status_code {res.status_code}')
 
-        records = self.convert_to_json(res)
-        transform_write_and_count(stream, records)
+            records = self.convert_to_json(res)
+            if len(records) == self.MAX_RECORDS:
+                raise ValueError('Number of records returned is equal to '
+                                 'hudson\'s limit. This means that we will be '
+                                 'missing data.')
+            transform_write_and_count(stream, records)
+
+            start_date = start_date.add(days=1)
+            if start_date == end_date:
+                request_config['run'] = False
+            else:
+                request_config['data'] = self.build_body(stream, start_date)
+
+    @staticmethod
+    def intialize_dates(self):
+        """Returns the dates a week before and a week after"""
+        today = pendulum.today('UTC')
+        week_earlier = today.subtract(weeks=1)
+        week_later = today.add(weeks=1)
+        return (week_earlier, week_later)
 
     def build_headers(self):
         return {
@@ -55,18 +81,17 @@ class HudsonltdExecutor(TapExecutor):
                            f'{self.config.get("pw")}').encode('ascii')
         return base64.b64encode(encoding_string).decode('utf-8')
 
-    def build_body(self, stream):
+    def build_body(self, stream, start_date):
         """Builds request in xml format"""
         stream_request_token = stream.stream_metadata['request-token']
-        today = pendulum.today('UTC')
-        week_earlier = today.subtract(weeks=1).to_date_string()
-        week_later = today.add(weeks=1).to_date_string()
+        start_date_str = start_date.to_date_string()
+        next_day = start_date.add(days=1).to_date_string()
         xml_string = f"""<?xml version='1.0'?>
 <ReadRecords>
     <SiteId>a15515</SiteId>
     <RequestToken>{stream_request_token}</RequestToken>
-    <FilterDate1>{week_earlier}</FilterDate1>
-    <FilterDate2>{week_later}</FilterDate2>
+    <FilterDate1>{start_date_str}</FilterDate1>
+    <FilterDate2>{next_day}</FilterDate2>
 </ReadRecords>"""
         return xml_string
 
